4f809fd3d02085f6bff1267c5c5ebde77845e19d
mrpatches123 <49619048+mrpatches123@users.noreply.github.com>
2026-06-19 11:10:16 -0400

    timer tests

--- scripts/tests/plugins/timer_test.js
+++ scripts/tests/plugins/timer_test.js
@@ -6,9 +6,16 @@ system.runInterval(() => {
         let timer = Timer.getFromEntity(player, TIMER_KEY);
         if (timer === false)
             return;
-        console.warn(Boolean(timer));
+        console.warn(Boolean(timer), timer === undefined);
         timer ??= new Timer();
-        console.warn({ TIMER_KEY, time: timer.getTime() });
-        timer.saveToEntity(player, TIMER_KEY);
+        const time = timer.getTime();
+        console.warn({ TIMER_KEY, time });
+        if (!time)
+            timer.saveToEntity(player, TIMER_KEY);
+        if (time > 1000) {
+            console.warn("restart");
+            timer.restart();
+            timer.saveToEntity(player, TIMER_KEY);
+        }
     });
 });
